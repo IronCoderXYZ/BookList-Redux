522a313146341563d6dfe7cdafc080aff4c80f60
Mitch Hankins <mitchhankins92@gmail.com>
2018-05-09 19:37:12 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import BookList from '../src/containers/bookList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BookList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JS: All The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'Howdy, How Are You?' },
+    { title: 'Just Another Book Title' }
+  ];
+}

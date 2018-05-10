8e8393b94f5919ba90bc6825bde0244411d6ddd7
Mitch Hankins <mitchhankins92@gmail.com>
2018-05-09 20:24:22 -0600

    Adding activeBook and selectBook implementation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import books from './books';
+import activeBook from './activeBook';
 
 const rootReducer = combineReducers({
-  books
+  books,
+  activeBook
 });
 
 export default rootReducer;
